c11e3c335ec4f4b9e5a88f9ea1f80290bdda7fc3
Jinserk Baik <823222+jinserk@users.noreply.github.com>
2023-09-27 10:18:31 -0400

    Prepare the base setup

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,6 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="reflex_app",
-    db_url="postgresql://postgres:postgres@postgres:5432/reflex",
+    app_name="basechemp",
+    db_url="postgresql://postgres:postgres@postgres:5432/basechemp",
 )
